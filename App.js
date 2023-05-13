90010a57616799effe500778d081a64474b9b34e
Cristiano Tamanini <126835320+cristiano-tamanini-plenor@users.noreply.github.com>
2023-05-13 10:59:37 -0300

    teste

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Text>Alterado tesste</Text>
       <StatusBar style="auto" />
     </View>
   );
